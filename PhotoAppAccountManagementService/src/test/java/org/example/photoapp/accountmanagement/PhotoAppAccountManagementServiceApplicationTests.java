8fa5931d49fa55cc5a80c865d1a81853613baacc
Cihan Biber <bibercihan@gmail.com>
2024-04-27 15:23:08 +0300

    Service Discovery, Users service init, AccountManagement service init

---
+++ PhotoAppAccountManagementService/src/test/java/org/example/photoapp/accountmanagement/PhotoAppAccountManagementServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package org.example.photoapp.accountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppAccountManagementServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
